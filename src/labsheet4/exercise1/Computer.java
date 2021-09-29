d23b365d01a5320e65884272770253c7cc0c7097
t00201634 <Bryan.OShea@students.ittralee.ie>
2021-09-29 09:28:40 +0100

    lab 4 ex 2 completed

---
+++ src/labsheet4/exercise1/Computer.java
@@ -0,0 +1,72 @@
+package labsheet4.exercise1;
+
+public class Computer {
+    private String manufacturer;
+    private String type;
+    private double speed;
+    private int ram;
+    private double price;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        if (speed > 0)
+            this.speed = speed;
+    }
+
+    public int getRam() { return ram; }
+
+    public void setRam(int ram) {
+        if (ram > 0)
+            this.ram = ram;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0)
+            this.price = price;
+    }
+
+    public String toString() {
+        return "Manufacturer: " + getManufacturer() + " Type: " + getType() + " Speed: " + getSpeed() + " RAM: "
+                + getRam() + " Price: " + getPrice();
+    }
+
+    public Computer() {
+        setManufacturer("Not available");
+        setType("Not available");
+        setSpeed(0);
+        setRam(0);
+        setPrice(0);
+//        shorthand: this("Not available","Not available",0,0,0);
+    }
+
+    public Computer(String manufacturer, String type, double speed, int ram, double price ) {
+        setManufacturer(manufacturer);
+        setType(type);
+        setSpeed(speed);
+        setRam(ram);
+        setPrice(price);
+    }
+}
