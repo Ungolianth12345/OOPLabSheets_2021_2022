c4a7ded4209d025e2f1792c32798d7e434219af9
t00201634 <Bryan.OShea@students.ittralee.ie>
2021-10-01 12:16:56 +0100

    finished lab 4 ex 4 finally

--- src/labsheet4/exercise4/Book_Test.java
+++ src/labsheet4/exercise4/Book_Test.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Book_Test {
+    public static String book_format(String a, double b, String c, int d) {
+        return String.format("%-25s%-8s%-13s%s", a, b, c, d + "\n\n");
+    }
+
     public static void main(String[] args) {
         String output = "";
 
@@ -14,33 +18,33 @@ public class Book_Test {
 
         Book favbook = new Book(ftitle, fprice, fisbn, fpages);
 
-        output += "Calling the multi-argument Book constructor. " +
-                "The first Book object details are: \n\n" + favbook.toString();
-
-        /*String ltitle = JOptionPane.showInputDialog("Please enter the title of your least favorite book");
+        String ltitle = JOptionPane.showInputDialog("Please enter the title of your least favorite book");
         double lprice = Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of your least favorite book"));
         String lisbn = JOptionPane.showInputDialog("Please enter the ISBN of your least favorite book");
         int lpages = Integer.parseInt(JOptionPane.showInputDialog("Please enter the number of pages of your least favorite book"));
 
         Book lbook = new Book(ltitle, lprice, lisbn, lpages);
 
-        output += "\n\nCalling the multi-argument Book constructor. " +
-                "The second Book object details are: \n\n" + lbook.toString();*/
-
         // for textarea use string.format + %s spacing in 25, 8, 15, ?? for the four columns
 
         Font font = new Font("monospaced", Font.PLAIN, 12);
 
-        output += "Title\tPrice\tISBN\tPages" + favbook;
+        String header = String.format("%-25s%-8s%-13s%s", "Title", "Price", "ISBN", "Pages\n\n");
 
-        JTextArea textArea = new JTextArea(output);
+        output += header;
+
+        output += book_format(favbook.getTitle(), favbook.getPrice(), favbook.getIsbn(), favbook.getPages());
+
+        output += book_format(lbook.getTitle(), lbook.getPrice(), lbook.getIsbn(), lbook.getPages());
+
+        JTextArea textArea = new JTextArea(5, 20);
 
         textArea.setFont(font);
 
         textArea.append(output);
 
-        JOptionPane.showMessageDialog(null,output,"Book " +
-                "Data",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, textArea,"Book " +
+            "Data",JOptionPane.INFORMATION_MESSAGE);
 
         System.exit(0);
     }
